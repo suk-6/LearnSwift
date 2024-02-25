639a47feafbca392084f2f05d75c0b336f47f4e0
suk-6 <me@suk.kr>
2024-02-25 22:09:34 +0900

    testing

--- LearnSwift/ContentView.swift
+++ LearnSwift/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .padding(1)
+            Text("Hello, World!")
         }
         .padding()
     }
